34095f122e60654113ef178f738864e3546eb3c5
Yedi278 <yehan278@gmail.com>
2025-01-08 19:31:46 +0100

    data tester

--- STM32 - Codici/9 - Data_Array/Presa Dati/test.py
+++ STM32 - Codici/9 - Data_Array/Presa Dati/test.py
@@ -2,18 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('dati_long.csv', sep=',', header=None, )
+df = pd.read_csv('Presa Dati/dati_long.csv', sep=',', header=None, )
 
 # Plot the data
 data = df[:].to_numpy(dtype=float)
 data = data.T
 print(data, data.shape)
 
-plt.hist(data, bins=1000)
+plt.hist(data, bins=1000, density=True)
 plt.title('Photon Histogram')
+plt.ylabel('Bin Counts')
+plt.xlabel('Amplitude [V]')
+plt.savefig('Data_Array_Photon_Histogram.png')
 plt.show()
 
-df = pd.read_csv('data_buffer.csv', sep=',', header=None)
+df = pd.read_csv('Presa Dati/data_buffer.csv', sep=',', header=None)
 data_2 = df[:].to_numpy(dtype=float)
 print(len(data_2[0]))
 
@@ -23,4 +26,5 @@ for i in range(3000):
 plt.title('Persistance View')
 plt.ylabel('Amplitude [V]')
 plt.xlabel('Time [200ns]')
+plt.savefig('Data_Array_Persistance_View.png')
 plt.show()
