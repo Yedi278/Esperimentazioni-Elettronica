3763fbf035a30978d51f64c66095866209814d77
Yedi278 <yehan278@gmail.com>
2025-01-16 15:24:16 +0100

    .

--- STM32 - Codici/10 - Dual ADC/BsGO/22Na/a.py
+++ STM32 - Codici/10 - Dual ADC/BsGO/22Na/a.py
@@ -7,3 +7,20 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 df = pd.read_csv('22Na_raw1.csv', sep=',', header=None)
 df2 = pd.read_csv('22Na_raw2.csv', sep=',', header=None)
+
+data1 = df.to_numpy(dtype=float)
+data2 = df2.to_numpy(dtype=float)
+
+data = np.append(data1, data2, axis=0)
+
+lx, rx = [],[]
+
+for i in range(data.shape[0]):
+    if data[i,15] > 0.15:
+        lx.append(data[i,15])
+    if data[i, 400+15] > 0.15:
+        rx.append(data[i, 400+15])
+
+plt.hist(lx, bins=400, density=True)
+plt.hist(rx, bins=400, density=True)
+plt.show()
